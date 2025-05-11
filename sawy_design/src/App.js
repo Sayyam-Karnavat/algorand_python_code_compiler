a818e6705f133c420a3b07626e689cf7c0f124a9
Sayyam-Karnavat <sanyam.karnavat5@gmail.com>
2025-05-11 20:16:46 +0530

    Leet Code like design changes to CSS and added for the editor to improve the UI.

---
+++ sawy_design/src/App.js
@@ -0,0 +1,11 @@
+import CodeEditor from './components/CodeEditor';
+
+function App() {
+  return (
+    <div className="App" style={{ height: "100vh", width: "100vw" }}>
+      <CodeEditor />
+    </div>
+  );
+}
+
+export default App;
